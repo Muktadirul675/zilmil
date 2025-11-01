758fd3e3fd0d58b28d6f325bdd5f0c35d8011da7
Muktadirul <muktadirul.05@gmail.com>
2025-11-01 12:22:06 +0000

    order lock

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -181,23 +181,23 @@ ASGI_APPLICATION = "backend.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('PG_NAME'),
-#         'USER': os.getenv('PG_USER'),
-#         'PASSWORD': os.getenv('PG_PASS'),
-#         'HOST': '161.248.201.221',  # or your VPS IP
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PG_NAME'),
+        'USER': os.getenv('PG_USER'),
+        'PASSWORD': os.getenv('PG_PASS'),
+        'HOST': '161.248.201.221',  # or your VPS IP
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
